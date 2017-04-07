516fad253c4403e8fb85e37ad3cd71e9ae77b51c
jipp <wolfgang.keller@wobilix.de>
2017-04-07 10:10:20 +0200

    added command for creating rfcomm

--- src/rfcomm.sh
+++ src/rfcomm.sh
@@ -1,3 +1,5 @@
-# create /dev/rfcomm0 for bluetooth paired device
+# create /dev/rfcomm0 for bluetooth paired device (mac address needed)
 
 #!/bin/sh
+
+sudo rfcomm bind /dev/rfcomm0 00:1B:10:10:1B:E4
